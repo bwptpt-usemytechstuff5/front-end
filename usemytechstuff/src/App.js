5d44c26559ea908cc6324ce32b79b7f29a5e0dbc
Kiran Junnarkar <kjunnarkar@yahoo.com>
2020-02-26 20:32:26 -0800

    Initial Commit

---
+++ usemytechstuff/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import RentContext from './contexts/RentContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
