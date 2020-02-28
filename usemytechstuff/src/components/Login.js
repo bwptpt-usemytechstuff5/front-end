28dc466a24537ed20c22e2831f079b7a4687b834
SFMack <sfcamack@icloud.com>
2020-02-27 20:42:51 -0800

    form basic structure complete

--- usemytechstuff/src/components/Login.js
+++ usemytechstuff/src/components/Login.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import styled from 'styled-components';
 
 const Login = () => {
+	const { handleSubmit, register, errors } = useForm();
+	const onSubmit = values => {
+		console.log(values);
+	};
 
-    return (
-        <div>
+	// styled components
+	const FormWrapper = styled.section`
+		width: 75vw;
+	`;
 
-        </div>
-    );
+	const StyledForm = styled.form``;
+
+	return (
+		<FormWrapper>
+			<StyledForm onSubmit={handleSubmit(onSubmit)}>
+				{/* eMail */}
+				<input
+					name='email'
+					ref={register({
+						required: 'Required',
+						pattern: {
+							value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+							message: 'invalid email address'
+						}
+					})}
+				/>
+				{errors.email && errors.email.message}
+
+				{/* username */}
+				<input
+					name='username'
+					ref={register({
+						validate: value => value !== 'admin' || 'Nice try!'
+					})}
+				/>
+				{errors.username && errors.username.message}
+
+				<button type='submit'>Submit</button>
+			</StyledForm>
+		</FormWrapper>
+	);
 };
 export default Login;
