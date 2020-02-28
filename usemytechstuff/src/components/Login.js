3f85dd332138c781bf0b3f1b5a80dbed70881db2
SFMack <sfcamack@icloud.com>
2020-02-27 21:05:53 -0800

    password field rendered based on registerNewUser state

--- usemytechstuff/src/components/Login.js
+++ usemytechstuff/src/components/Login.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
 
 const Login = () => {
+	// form state to check if the user is trying to register or login
+	const [registerNewUser, setRegisterNewUser] = useState(false);
+
+	// form structural components
 	const { handleSubmit, register, errors } = useForm();
+
+	// helper functions
 	const onSubmit = values => {
 		console.log(values);
 	};
@@ -38,6 +44,20 @@ const Login = () => {
 						validate: value => value !== 'admin' || 'Nice try!'
 					})}
 				/>
+				{errors.username && errors.username.message}
+
+				{/* 
+                if this person is trying to register a new 
+                user then render a password input field as well 
+                */}
+				{registerNewUser === true ? (
+					<input
+						name='password'
+						ref={register({
+							validate: value => value > 8 || 'Password must be longer then 8'
+						})}
+					/>
+				) : null}
 				{errors.username && errors.username.message}
 
 				<button type='submit'>Submit</button>
